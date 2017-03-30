c70ecd60c4921f72aca12d52dd5e8f272047a02d
95erlong <117000082@qq.com>
2017-03-30 11:28:20 +0800

    Initial Commit

---
+++ AlternateIconSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlternateIconSample
+//
+//  Created by Leo on 2017/3/30.
+//  Copyright © 2017年 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlternateIconSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AlternateIconSample
+//
+//  Created by Leo on 2017/3/30.
+//  Copyright © 2017年 Leo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
